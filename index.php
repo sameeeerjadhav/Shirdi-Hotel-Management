3efacee6b1c045bad96fcfa506d9af7c622af206
Sameer Jadhav <sameerpjadhav12@gmail.com>
2026-06-06 20:18:38 +0530

    Add root index.php for Hostinger shared hosting fallback

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Hostinger Shared Hosting Entry Point
+ * This file allows the application to run correctly when the document root
+ * is set to the project root instead of the 'public' folder.
+ */
+
+require __DIR__ . '/public/index.php';
